69b6ee292b98e592c1d5b6f066433c9d8f0de173
Ercel Peter Sotto <ercelpetersotto@gmail.com>
2019-03-28 09:19:06 +1300

    Modified CORS policy to accept HTTP Requests from Angular frontend. Also made minor change on datatype within Update SubCategory DataAccess method.

--- ItemManagementService/App_Start/WebApiConfig.cs
+++ ItemManagementService/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ItemManagementService
 {
@@ -13,6 +14,9 @@ namespace ItemManagementService
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(cors);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
