c950f96d06ad64551d70567f7d4c2c68e7f25ace
Pavel Isupov <exride@gmail.com>
2019-05-13 21:48:52 +1200

    Cat and Sub-cat list does not update when add or delete a new item of Category Management: resolved

--- IMSRepository/Utilities/ICategoryDataAccess.cs
+++ IMSRepository/Utilities/ICategoryDataAccess.cs
@@ -5,7 +5,7 @@ namespace IMSRepository.Utilities
     public interface ICategoryDataAccess
     {
         List<Category> GetCategories();
-        string InsertNewCategory(Category cat);
+        Category InsertNewCategory(Category cat);
         string UpdateCategoryDetails(Category cat);
         string DeleteCategory(int categoryId);
     }
